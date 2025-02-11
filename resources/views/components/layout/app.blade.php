897c38e2b8676edc63b6d294096ca70fd9c6d6d1
Gabriel Maeda <gabriel.maeda@crmall.com.br>
2025-02-11 12:50:18 -0300

    Change favicon

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -5,12 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Dashboard</title>
+    <title>Expenses tracker</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
 
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
